88803dd268c24e3cbddb2b9f491b22ef799b8c4a
changeseok <changseok@esmec.com>
2022-07-25 13:46:56 +0900

    coding-test: Going to shcool - registration

---
+++ src/main/java/com/code/codingtest/dynamicprogramming/GoingToSchool.java
@@ -0,0 +1,17 @@
+package com.code.codingtest.dynamicprogramming;
+
+public class GoingToSchool {
+    public static void main(String[] args) {
+        int width = 0;
+        int height = 0;
+        int[][] puddles = {{2,2}};
+
+        System.out.println(new GoingToSchool().solution(width, height, puddles));
+    }
+
+    public int solution(int width, int height, int[][] puddles) {
+
+
+        return 0;
+    }
+}
