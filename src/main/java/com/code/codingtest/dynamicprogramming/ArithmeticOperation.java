23a35d43a0576d9b89fbf56c1b8185587c99383b
steptoreal <jcob0920@gmail.com>
2022-07-27 17:20:19 +0900

    coding test : 4칙연산 등록

---
+++ src/main/java/com/code/codingtest/dynamicprogramming/ArithmeticOperation.java
@@ -0,0 +1,14 @@
+package com.code.codingtest.dynamicprogramming;
+
+public class ArithmeticOperation {
+    public static void main(String[] args) {
+        String[] arr = {"1", "-", "3", "+", "5", "-", "8"}; //1
+        String[] arr2 = {"5", "-", "3", "+", "1", "+", "2", "-", "4"}; //3
+
+        System.out.println(new ArithmeticOperation().solution(arr));
+    }
+
+    public int solution(String arr[]) {
+        return 0;
+    }
+}
