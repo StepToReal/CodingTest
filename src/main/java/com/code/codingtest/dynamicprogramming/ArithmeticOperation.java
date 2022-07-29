ddbaf845149fe14307114a58adc5d5ed4962a926
steptoreal <jcob0920@gmail.com>
2022-07-29 17:13:35 +0900

    coding test : 4칙연산 기존 solve 주석 추가 및 Min Max 값 판단 상수 변경

--- src/main/java/com/code/codingtest/dynamicprogramming/ArithmeticOperation.java
+++ src/main/java/com/code/codingtest/dynamicprogramming/ArithmeticOperation.java
@@ -5,11 +5,10 @@ public class ArithmeticOperation {
     char[] oper;
     int[] nums;
     String[] arr;
-    int temp = 987654321;
 
     public static void main(String[] args) {
-        String[] arr = {"1", "-", "3", "+", "5", "-", "8"}; //1
-        String[] arr2 = {"5", "-", "3", "+", "1", "+", "2", "-", "4"}; //3
+        String[] arr2 = {"1", "-", "3", "+", "5", "-", "8"}; //1
+        String[] arr = {"5", "-", "3", "+", "1", "+", "2", "-", "4"}; //3
 
         System.out.println(new ArithmeticOperation().solution(arr));
     }
@@ -28,8 +27,8 @@ public class ArithmeticOperation {
 
         for (int i = 0; i < n + 1; i++) {
             for (int j = 0; j < n + 1; j++) {
-                dp[0][i][j] = -1 * temp;
-                dp[1][i][j] = temp;
+                dp[0][i][j] = Integer.MIN_VALUE; //최대값이 저장될 테이블
+                dp[1][i][j] = Integer.MAX_VALUE;      //최소값이 저장될 테이블
             }
         }
 
@@ -37,25 +36,25 @@ public class ArithmeticOperation {
         oper = new char[n];
 
         for (int i = 0; i < arr.length; i++) {
-            if (i % 2 == 0) nums[i / 2] = Integer.parseInt(arr[i]);
-            else oper[i / 2] = arr[i].charAt(0);
+            if (i % 2 == 0) nums[i / 2] = Integer.parseInt(arr[i]); //arr 배열에 짝수는 숫자 -> nums 에 저장
+            else oper[i / 2] = arr[i].charAt(0);                    //arr 배열에 홀수는 연산자 -> oper 에 저장
         }
     }
 
-    private int solve(int flag, int start, int end) {
+    private int solve(int flag, int start, int end) {  //처음에는 0, 0, arr.length/2 값이 들어온다. 배열이기 때무에 arr.length/2 면 끝까지 가능
         //범위가 숫자 하나로 일치하는 경우
         if (start == end) {
             return dp[flag][start][end] = nums[start];
         }
 
         //이미 해결했던 문제라면
-        if (flag == 0 && dp[flag][start][end] != -1 * temp)
+        if (flag == 0 && dp[flag][start][end] != Integer.MIN_VALUE)
             return dp[flag][start][end];
-        if (flag == 1 && dp[flag][start][end] != temp)
+        if (flag == 1 && dp[flag][start][end] != Integer.MAX_VALUE)
             return dp[flag][start][end];
 
         //flag에 따라 초기값을 다르게 줌
-        int ret = (flag == 0) ? -1 * temp : temp;
+        int ret = (flag == 0) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
         //방문체크
         dp[flag][start][end] = 0;
 
@@ -77,6 +76,7 @@ public class ArithmeticOperation {
                     //두 구간 사이의 연산자가 -일때, 최소 - 최대
                     ret = Math.min(ret, solve(1, start, mid) - solve(0, mid + 1, end));
                 } else {
+                    //두 구간 사이의 연산자가 +일때, 최소 + 최소
                     ret = Math.min(ret, solve(1, start, mid) + solve(1, mid + 1, end));
                 }
             }
