e439efb12e5ab7263a6983ce47068dd3cee907a2
steptoreal <jcob0920@gmail.com>
2022-08-17 09:43:22 +0900

    DFS, BFS 알고리즘 샘플 소스 구현

---
+++ src/main/java/com/code/algorithm/BfsExample01.java
@@ -0,0 +1,52 @@
+package com.code.algorithm;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class BfsExample01 {
+
+}
+
+class BfsExampleGraph {
+    private int v;
+    private LinkedList<Integer>[] adj;
+
+    BfsExampleGraph(int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+
+        for (int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    private void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    private void bfs(int s) {
+        boolean[] visited = new boolean[this.v];//방문여부 확인용 변수
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        visited[s] = true;
+        queue.add(s);
+
+        while (queue.size() != 0) {
+            //방문한 노드를 큐에서 추출하고 값을 출력
+            s = queue.poll();
+            System.out.println(s + " ");
+
+            //방문한 노드와 인접한 모든 노드를 가지고 온다.
+            Iterator<Integer> i = adj[s].listIterator();
+            while (i.hasNext()) {
+                int n = i.next();
+
+                //방문하지 않은 노드면 방문한 것으로 표시하고 큐에 삽입
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/code/algorithm/DfsExample01.java
@@ -0,0 +1,49 @@
+package com.code.algorithm;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class DfsExample01 {
+}
+
+class DfsExampleGraph {
+    private int v;
+    private LinkedList<Integer>[] adj;
+
+    DfsExampleGraph(int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+
+        //인접 리스트 초기화
+        for (int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    private void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    private void dfs(int v) {
+        boolean visited[] = new boolean[this.v];
+
+        //v를 시작하는 노드로 dfsUtil 호출
+        dfsUtil(v, visited);
+    }
+
+    private void dfsUtil(int v, boolean visited[]) {
+        //현재 노드를 방문한 것으로 표시하고 값을 출력
+        visited[v] = true;
+        System.out.println(v + " ");
+
+        //방문한 노드와 인접한 모든 노드를 가지고 온다.
+        Iterator<Integer> it = adj[v].listIterator();
+        while (it.hasNext()) {
+            int n = it.next();
+            //방무하지 않은 노드면 해당 노드를 시작 노드로 다시 dfsUtil 재귀 호출
+            if (!visited[n]) {
+                dfsUtil(n, visited);
+            }
+        }
+    }
+}
