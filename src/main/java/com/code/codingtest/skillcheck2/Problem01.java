b0579fdcbec6963b77804d81174cb22d75add904
steptoreal <jcob0920@gmail.com>
2022-09-23 08:46:28 +0900

    스킬체크 lv1,2 solve

---
+++ src/main/java/com/code/codingtest/skillcheck2/Problem01.java
@@ -0,0 +1,57 @@
+package com.code.codingtest.skillcheck2;
+
+import java.util.*;
+
+public class Problem01 {
+    public static void main(String[] args) {
+        int[] progresses = {95, 90, 99, 99, 80, 99};
+        int[] speeds = {1, 1, 1, 1, 1, 1};
+
+        System.out.println(Arrays.toString(new Problem01().solution(progresses, speeds)));
+    }
+
+    private int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> answerList = new ArrayList<>();
+        int[] answer;
+        int[] periods = new int[progresses.length];
+
+        for (int i = 0; i < progresses.length; i++) {
+            if ((100 - progresses[i]) % speeds[i] == 0) {
+                periods[i] = (100 - progresses[i]) / speeds[i];
+            } else {
+                periods[i] = ((100 - progresses[i]) / speeds[i]) + 1;
+            }
+        }
+
+        for (int i = 0; i < periods.length; i++) {
+            int deployCount = 1;
+
+            if (i == periods.length - 1) {
+                answerList.add(deployCount);
+            }
+
+            for (int j = i + 1; j < periods.length; j++){
+                if (periods[i] >= periods[j]) {
+                    deployCount++;
+
+                    if (j == periods.length - 1) {
+                        answerList.add(deployCount);
+                        i = j;
+                    }
+                } else {
+                    answerList.add(deployCount);
+                    i = j - 1;
+                    break;
+                }
+            }
+        }
+
+        answer = new int[answerList.size()];
+
+        for (int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
+        }
+
+        return answer;
+    }
+}
---
+++ src/main/java/com/code/codingtest/skillcheck2/Problem02.java
@@ -0,0 +1,63 @@
+package com.code.codingtest.skillcheck2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Problem02 {
+    public static void main(String[] args) {
+        String s = "{{1,2,3},{2,1},{1,2,4,3},{2}}";
+
+        System.out.println(Arrays.toString(new Problem02().solution(s)));
+    }
+
+    public int[] solution(String s) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> answerList = new ArrayList<>();
+        int[] answer;
+        s = s.substring(1, s.length() - 1);
+        String[] sArr = s.split("}");
+
+        for (int i = 0; i < sArr.length; i++) {
+            sArr[i] = sArr[i].replaceAll("\\{","");
+        }
+
+        for (String s1 : sArr) {
+            String[] s1Arr = s1.split(",");
+            List<Integer> innerList = new ArrayList<>();
+
+            for (int i = 0; i < s1Arr.length; i++) {
+                if (!s1Arr[i].isEmpty()) {
+                    int value = Integer.parseInt(s1Arr[i]);
+                    innerList.add(value);
+                }
+            }
+
+            list.add(innerList);
+        }
+
+        list.sort(new Comparator<List<Integer>>() {
+            @Override
+            public int compare(List<Integer> o1, List<Integer> o2) {
+                return o1.size() - o2.size();
+            }
+        });
+
+        for (List<Integer> innerList : list) {
+            for (int value : innerList) {
+                if (!answerList.contains(value)) {
+                    answerList.add(value);
+                }
+            }
+        }
+
+        answer = new int[answerList.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = answerList.get(i);
+        }
+
+        return answer;
+    }
+}
