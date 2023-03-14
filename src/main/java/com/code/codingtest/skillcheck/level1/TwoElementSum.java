48d39b048f2cc47e64c47a285e1c9fefe4ea0736
steptoreal <jcob0920@gmail.com>
2023-03-14 17:28:05 +0900

    프로그래머스 테스트 풀이 등록
    - lv1. 두 개 뽑아서 더하기 (backtracking 연습 v2)

--- src/main/java/com/code/codingtest/skillcheck/level1/TwoElementSum.java
+++ src/main/java/com/code/codingtest/skillcheck/level1/TwoElementSum.java
@@ -1,23 +1,23 @@
 package com.code.codingtest.skillcheck.level1;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class TwoElementSum {
     public static void main(String[] args) {
         int[] numbers = {2, 1, 3, 4, 1};
-        System.out.println(Arrays.toString(new TwoElementSum().solution(numbers)));
+        System.out.println(Arrays.toString(new TwoElementSum().solution2(numbers)));
     }
 
     int[] numbers;
     int[] temp = new int[2];
-    Set<Integer> set = new HashSet<>();
+    Set<Integer> set = new TreeSet<>();
 
     public int[] solution(int[] numbers) {
         this.numbers = numbers;
         bt(0, 0);
-        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+        return set.stream().mapToInt(Integer::intValue).toArray();
     }
 
     private void bt(int at, int depth) {
@@ -30,6 +30,22 @@ public class TwoElementSum {
         for (int i = at; i < numbers.length; i++) {
             temp[depth] = numbers[i];
             bt(i + 1, depth + 1);
+        }
+    }
+
+    public int[] solution2(int[] numbers) {
+        bt2(numbers, 0, 0, 0);
+        return set.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private void bt2(int[] numbers, int at, int depth, int sum) {
+        if (depth == 2) {
+            set.add(sum);
+            return;
+        }
+
+        for (int i = at; i < numbers.length; i++) {
+            bt2(numbers, i + 1, depth + 1, sum + numbers[i]);
         }
     }
 }
